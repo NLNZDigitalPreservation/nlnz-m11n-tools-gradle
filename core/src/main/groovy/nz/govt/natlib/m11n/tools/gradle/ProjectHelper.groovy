6015b2ab157abc4277ed055b33f1b48e9e16e6a8
Kurt Lenfesty <Kurt.Lenfesty.NZ@gmail.com>
2018-10-22 11:58:12 +1300

    Correct logic in ProjectHelper.hasAnyTasks.
    - Correct logic so that ProjectHelper#hasAnyTasks correctly finds
      whether the named list of tasks has been called.

--- core/src/main/groovy/nz/govt/natlib/m11n/tools/gradle/ProjectHelper.groovy
+++ core/src/main/groovy/nz/govt/natlib/m11n/tools/gradle/ProjectHelper.groovy
@@ -9,7 +9,7 @@ class ProjectHelper {
      * Indicates if the given project (or its parent if it is a subproject) has any tasks with the given name.
      * Subproject tasks have names starting with {@code <subproject-name>:<task-name>}.
      */
-    static boolean hasAnyTasks(Project project) {
+    static boolean hasAnyTasks(Project project, List<String> taskNames) {
         boolean isSubproject = (project != project.rootProject)
         String subprojectPrefix = isSubproject ? "${project.name}:" : ""
         List<TaskExecutionRequest> taskExecutionRequestList = project.gradle.getStartParameter().getTaskRequests()
@@ -17,7 +17,9 @@ class ProjectHelper {
             taskExecutionRequest.args.any { argument ->
                 // it's either a global task (root project and all subprojects (no ':')) or
                 // it's a subproject and it starts with the subproject name
-                (argument.indexOf(":") < 0) || (isSubproject && argument.indexOf(subprojectPrefix) == 0)
+                taskNames.any { String candidateTaskName ->
+                    (argument == candidateTaskName) || (argument == "${subprojectPrefix}${candidateTaskName}")
+                }
             }
         }
     }
